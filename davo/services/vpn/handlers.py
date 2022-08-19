8c40d231d5eeedd471ca49d7207cd3bfb382f504
David Jhanyan <david.jhanyan@gatech.global>
2022-08-19 10:02:20 +0300

    Add new vpn handlers

--- davo/services/vpn/handlers.py
+++ davo/services/vpn/handlers.py
@@ -9,7 +9,13 @@ from . import utils
 
 C_OPENCONNECT = (
     '{{ echo \'{}\' ; echo \'{}\' ; }} | sudo openconnect --user={}'
-    ' --authgroup={} {} --passwd-on-stdin'
+    ' --authgroup={} {} --passwd-on-stdin --no-dtls'
+    ' --script=/home/davo/Dropbox/etc/vpnc-script'
+)
+C_OPENCONNECT_SUDO = (
+    'sudo openconnect --user={}'
+    ' --authgroup={} {} --passwd-on-stdin --no-dtls'
+    ' --script=/home/davo/Dropbox/etc/vpnc-script'
 )
 C_OPENVPN = (
     'sudo openvpn --config {} --auth-user-pass {} --auth-retry interact'
@@ -27,7 +33,24 @@ def connect_openconnect(account):
     os.system(cmd)
 
 
-def connect_openvpn(account):
+def connect_openconnect_sudo(account):
+    print('Connecting to `{}`'.format(account['auth_group']))
+    cmd = C_OPENCONNECT_SUDO.format(
+        account['user'], account['auth_group'], account['url'])
+    print(cmd)
+
+    otp = pyotp.TOTP(account['otp_secret']).now()
+    sudo = davo.utils.conf.load_kr_kp_pass()
+
+    ch = pexpect.spawn(cmd)
+    ch.expect('\[sudo\] ', timeout=1)
+    ch.sendline(sudo)
+    ch.sendline(account['password'])
+    ch.sendline(otp)
+    ch.interact()
+
+
+def connect_openvpn_otp(account):
     otp = pyotp.TOTP(account['otp_secret']).now()
 
     pass_file = utils.create_temp_file('{}\n{}'.format(
@@ -46,4 +69,20 @@ def connect_openvpn(account):
         utils.remove_temp_file()
 
     ch.sendline(otp)
+    ch.interact()
+
+
+def connect_openvpn(account):
+    pass_file = utils.create_temp_file('{}\n{}'.format(
+        account['user'], account['password']))
+    cmd = C_OPENVPN.format(account['openvpn_conf_path'], pass_file)
+
+    sudo = davo.utils.conf.load_kr_kp_pass()
+
+    try:
+        ch = pexpect.spawn(cmd)
+        ch.expect('\[sudo\] password for', timeout=1)
+        ch.sendline(sudo)
+    finally:
+        utils.remove_temp_file()
     ch.interact()
